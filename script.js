f57ca9109a233b2d4f335da9d54d9906d8fa9ed9
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2024-03-29 00:43:30 +0200

    Add API integration to fetch quotes

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+let apiQuotes = [];
 
+// Get Quotes From API
+// Define an asynchronous function named getQuote
+async function getQuote() {
+  // Set the URL of the API from which we want to fetch quotes
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+
+  try {
+    // Use the fetch API to make a request to the apiUrl
+    const response = await fetch(apiUrl);
+
+    // Wait for the response to come back and convert it to JSON
+    // Assign the resulting data to the variable apiQuotes
+    apiQuotes = await response.json();
+
+    // Log the quotes data to the console
+    console.log(apiQuotes);
+  } catch (error) {
+    // If there's an error in the try block, it will be caught here
+
+    // Log a custom error message and the error itself to the console
+    console.log("Whoops, no quote", error);
+  }
+}
+
+// On Load
+getQuote();

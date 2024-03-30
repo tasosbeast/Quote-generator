714b29e75619071284d8e244d639978402e6b502
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2024-03-31 00:25:36 +0200

    Add tweet functionality to script.js

--- script.js
+++ script.js
@@ -57,5 +57,10 @@ async function getQuote() {
   }
 }
 
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
 // On Load
 getQuote();
